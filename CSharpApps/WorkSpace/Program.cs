82bf86ea19d13d8a908d042b6652b2217c1c5a38
mhmt84 <mahmutkudeyt@gmail.com>
2022-02-05 14:14:24 +0300

    Create WorkSpace

---
+++ CSharpApps/WorkSpace/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace WorkSpace
+
+{
+    class Program
+
+    { static void Main(string[] args)
+        {
+
+            Console.WriteLine("Merhaba WorkSpace!");
+            Console.WriteLine("Bu ilk uygulamamız; ");
+            Console.ReadLine();
+        }
+    
+    
+    
+    
+    }
+
+
+}
+
+
+
+
